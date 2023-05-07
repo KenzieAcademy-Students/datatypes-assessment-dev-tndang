ad7a604492e819b06d2453624a8202d9ffef69a7
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-05-07 02:42:45 -0400

    Did not save nefore last commit, refer to that note.

--- code.js
+++ code.js
@@ -29,7 +29,39 @@ let dataAnalysis = function() {
 }
 
 let renderResults = function() {
-    results.innerText = "Test";
+    //Create two divs, one to hold the Text Analysis Title, and one to Hold the Data
+    let textAnalysisDiv = document.createElement(`div`);
+    let h2Element = document.createElement(`h2`);
+    results.append(textAnalysisDiv);
+    textAnalysisDiv.append(h2Element)
+    h2Element.innerText = `Text Analysis`;
+
+    let dataResultsDiv = document.createElement(`div`);
+    results.append(dataResultsDiv);
+    
+    //Create two divs withing the bottom div to make two columns
+    let resultsLeftDiv = document.createElement(`div`);
+    dataResultsDiv.append(resultsLeftDiv);
+
+    let resultsRightDiv = document.createElement(`div`);
+    dataResultsDiv.append(resultsRightDiv);
+
+    //Create two divs within the following first div to hold the Vowel and Punctuation Counts
+    let vowelCountDiv = document.createElement(`div`);
+    let pElement1 = document.createElement(`p`);
+    resultsLeftDiv.append(vowelCountDiv);
+    vowelCountDiv.append(pElement1);
+    pElement1.innerText = 
+        `
+        a: 0 \n
+        e: 0 \n
+        i: 0 \n
+        o: 0 \n
+        u: 0
+        `
+    ;
+    let punctuationCountDiv = document.createElement(`div`);
+    resultsLeftDiv.append(punctuationCountDiv);
 }
 
 textArea.addEventListener("keyup", dataAnalysis);
