8f737e3ab966622fd8eab72d822abd9ed52eac10
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-05-07 06:42:12 +0000

    deploy: 94c70e566b9fac922f15e27a349ead961adbf1a9

--- code.js
+++ code.js
@@ -2,3 +2,34 @@ let textArea = document.getElementById("text");
 let results = document.getElementById("results");
 
 // Your Code Here.
+let analyzedData = {
+    vowels: {
+        a: 0,
+        e: 0, 
+        i: 0,
+        o: 0,
+        u: 0,
+    },
+    punctuationMarks: {
+        period: 0,
+        comma: 0,
+        questionMark: 0,
+        exclamationPoint: 0,
+    },
+    numberOfCharacters: 0,
+    numnerOfWords: 0,
+    longestWord: "",
+    shortestWord: "",
+    last3Words: [],
+    waldoOccuranceIndex: [],
+}
+
+let dataAnalysis = function() {
+    renderResults();
+}
+
+let renderResults = function() {
+    results.innerText = "Test";
+}
+
+textArea.addEventListener("keyup", dataAnalysis);
