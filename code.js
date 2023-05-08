def67978a58abf51aec100a5f90e1f2bf9b8f5c5
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-05-07 23:14:31 -0400

    Had to reconsider my approach on this, adjusted the renderer function to take in the result value to render.

--- code.js
+++ code.js
@@ -2,75 +2,37 @@ let textArea = document.getElementById("text");
 let results = document.getElementById("results");
 
 // Your Code Here.
-let analyzedData = {
+let result = {
     vowels: {
-        a: 0,
-        e: 0, 
-        i: 0,
-        o: 0,
-        u: 0,
+      a: 0,
+      e: 0,
+      i: 0,
+      o: 0,
+      u: 0
     },
-    punctuationMarks: {
-        period: 0,
-        comma: 0,
-        questionMark: 0,
-        exclamationPoint: 0,
+    punctuation: {
+      period: 0,
+      comma: 0,
+      exclamation: 0,
+      questionMark: 0
     },
-    numberOfCharacters: 0,
-    numnerOfWords: 0,
+    numCharacters: 0,
+    numWords: 0,
     longestWord: "",
     shortestWord: "",
-    last3Words: [],
-    waldoOccuranceIndex: [],
+    lastThreeWords: [],
+    waldoIndexes: [],
+  }
+
+let vowelAnalyzer = function(text) {
+    for (let index = 0; index < text.length; index++) {
+        if (text.charAt(index) == `a`) {
+            result.vowels.a += 1;
+        } 
+    }
 }
 
-let vowelCounter = function() {
-    let vowels = {
-        a: 0,
-        e: 0,
-        i: 0,
-        o: 0,
-        u: 0,
-    };
-}
-
-let punctuationCounter = function() {
-    let punctuation = {
-        period: 0,
-        comma: 0,
-        questionMark: 0,
-        exclamation: 0,
-    };
-}
-
-let characterCounter = function() {
-    let numCharacters = 0;
-}
-
-let wordCounter = function() {
-    let numWords = 0;
-}
-
-let longWordDeterminer = function() {
-    let longestWord = ``;
-}
-
-let shortWordDeterminer = function() {
-    let shortestWord = ``;
-}
-
-let last3WordsDeterminer = function() {
-    let last3Words = ``;
-}
-
-let waldoIndexDeterminer = function() {
-    let waldoIndexes = ``;
-}
-
-let dataAnalysis = function() {
-}
-
-let renderResults = function() {
+let dataRenderer = function(resultData) {
     //Create two divs, one to hold the Text Analysis Title, and one to Hold the Data
     let textAnalysisDiv = document.createElement(`div`);
     let h2Element = document.createElement(`h2`);
@@ -96,15 +58,15 @@ let renderResults = function() {
     let vowelsElement = document.createElement(`p`);
     resultsLeftDiv.append(vowelTitleElement);
     resultsLeftDiv.append(vowelsElement);
-    vowelTitleElement.innerText = `Vowel Counts`
-    vowelsElement.innerText = `a: 0 \n e: 0 \n i: 0 \n o: 0 \n u: 0`;
+    vowelTitleElement.innerText = `Vowel Counts`;
+    vowelsElement.innerText = `a: ${resultData.vowels.a} \n e: ${resultData.vowels.e} \n i: ${resultData.vowels.i} \n o: ${resultData.vowels.o} \n u: ${resultData.vowels.u}`;
 
     let punctuationTitleElement = document.createElement(`h3`);
     let punctuationTextElement = document.createElement(`p`);
     resultsLeftDiv.append(punctuationTitleElement);
     resultsLeftDiv.append(punctuationTextElement);
     punctuationTitleElement.innerText = `Punctuation Counts`
-    punctuationTextElement.innerText = `Periods: 0 \n Commas: 0 \n Question Marks: 0 \n Exclamations: 0`;
+    punctuationTextElement.innerText = `Periods: ${resultData.punctuation.period} \n Commas: ${resultData.punctuation.comma} \n Question Marks: ${resultData.punctuation.questionMark} \n Exclamations: ${resultData.punctuation.exclamation}`;
 
     //Create the elements within the right div to hold the rest of the data counts
     let numCharactersElement = document.createElement(`h3`);
@@ -114,18 +76,18 @@ let renderResults = function() {
     let last3WordsElement = document.createElement(`h3`);
     let waldoCountElement = document.createElement(`h3`);
     resultsRightDiv.append(numCharactersElement);
-    numCharactersElement.innerText = `Number of Characters: 0`;
+    numCharactersElement.innerText = `Number of Characters: ${resultData.numCharacters}`;
     resultsRightDiv.append(numWordsElement);
-    numWordsElement.innerText = `Number of Words: 0`;
+    numWordsElement.innerText = `Number of Words: ${resultData.numWords}`;
     resultsRightDiv.append(longestWordElement);
-    longestWordElement.innerText = `Longest Word: `;
+    longestWordElement.innerText = `Longest Word: ${resultData.longestWord}`;
     resultsRightDiv.append(shortestWordElement);
-    shortestWordElement.innerText = `Shortest Word: `;
+    shortestWordElement.innerText = `Shortest Word: ${resultData.shortestWord}`;
     resultsRightDiv.append(last3WordsElement);
-    last3WordsElement.innerText = `Last Three Words: `;
+    last3WordsElement.innerText = `Last Three Words: ${resultData.lastThreeWords}`;
     resultsRightDiv.append(waldoCountElement);
-    waldoCountElement.innerText = `Waldo Indexes: [ ]`;
+    waldoCountElement.innerText = `Waldo Indexes: ${resultData.waldoIndexes}`;
 }
 
-renderResults();
-textArea.addEventListener("keyup", dataAnalysis);
+textArea.addEventListener("keyup", vowelAnalyzer);
+dataRenderer(result);
